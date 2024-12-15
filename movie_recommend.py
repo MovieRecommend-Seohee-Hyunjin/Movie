fd62cd522ec3bf744b4dc472e6e81c7f7977b41a
Kim hyunjin <hyun020227@naver.com>
2024-12-15 14:23:39 +0900

    파이썬 수정

--- movie_recommend.py
+++ movie_recommend.py
@@ -44,5 +44,50 @@ def genre(genre_name):
     return render_template('genre.html', genre_name=genre_name, movies=movies)
 
 
+
+# 연대별 영화 전체 보기
+@app.route('/Date_all/')
+def showDate_all():
+    conn = sqlite3.connect('movie_recommend.db')
+    cursor = conn.cursor()
+
+    # 연대별 영화 가져오기
+    decades = ['2000s', '2010s', '2020s']
+    decade_movies = {}
+    for decade in decades:
+        if decade == '2000s':
+            cursor.execute("SELECT Name, Year FROM movie WHERE Year BETWEEN 2000 AND 2009 LIMIT 3")
+        elif decade == '2010s':
+            cursor.execute("SELECT Name, Year FROM movie WHERE Year BETWEEN 2010 AND 2019 LIMIT 3")
+        elif decade == '2020s':
+            cursor.execute("SELECT Name, Year FROM movie WHERE Year BETWEEN 2020 AND 2029 LIMIT 3")
+        decade_movies[decade] = [{'Name': row[0], 'Year': row[1]} for row in cursor.fetchall()]
+
+    conn.close()
+    return render_template('Date_all.html', decades=decades, decade_movies=decade_movies)
+
+# 특정 연대의 영화 보기
+@app.route('/Date/<decade>')
+def showDate(decade):
+    conn = sqlite3.connect('movie_recommend.db')
+    cursor = conn.cursor()
+
+    # 특정 연대의 영화 가져오기
+    if decade == '2000s':
+        cursor.execute("SELECT Name, Year FROM movie WHERE Year BETWEEN 2000 AND 2009")
+    elif decade == '2010s':
+        cursor.execute("SELECT Name, Year FROM movie WHERE Year BETWEEN 2010 AND 2019")
+    elif decade == '2020s':
+        cursor.execute("SELECT Name, Year FROM movie WHERE Year BETWEEN 2020 AND 2029")
+    else:
+        return "Invalid decade", 404
+
+    movies = [{'Name': row[0], 'Year': row[1]} for row in cursor.fetchall()]
+
+    conn.close()
+    return render_template('Date.html', decade=decade, movies=movies)
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
